268f6d68dafd7c60e68533ea635535732036dbb0
ratzeni <rossano.atzeni@crs4.it>
2022-05-11 16:46:28 +0200

    Demo - call

--- config/create_paths.sh
+++ config/create_paths.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#mkdir -p /volumes/workdir
-#mkdir -p /volumes/workdir/inputs
-#mkdir -p /volumes/workdir/results
+mkdir -p /volumes/workdir
+mkdir -p /volumes/workdir/inputs
+mkdir -p /volumes/workdir/outputs
+mkdir -p /volumes/workdir/results
 #mkdir -p /volumes/workdir/resources
 #mkdir -p /volumes/workdir/workflow
 mkdir -p /volumes/logs
 mkdir -p /volumes/tmp
 
-#chown -R appuser /volumes/workdir
+chown -R appuser /volumes/workdir
 chown -R appuser /volumes/logs
 chown -R appuser /volumes/tmp
 chmod +x /code/src/bin/musta
